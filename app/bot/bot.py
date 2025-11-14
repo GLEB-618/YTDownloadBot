69b340f92df32c92a13481128408aae6a58324e5
gleb <vandal2006a@gmail.com>
2025-11-14 23:55:15 +0300

    [+] docker tg bot api

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.client.telegram import TelegramAPIServer
 from aiogram.enums import ParseMode
 from app.bot.router import router
 from app.core.config import settings
@@ -10,7 +12,9 @@ logger = get_logger(__name__, "logs.log")
 
 async def run_bot():
     try:
-        bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+        local_server = TelegramAPIServer.from_base("http://127.0.0.1:8081")
+        session = AiohttpSession(api=local_server)
+        bot = Bot(token=settings.BOT_TOKEN, session=session, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
         dp = Dispatcher() 
 
         dp.include_routers(router)
